6f651b5cdba38bc429c722cc8646b5a37d0512c2
Himanshu <himanshuramchandani08.gmail.com>
2025-07-31 20:34:44 +0530

    day 3 modules and packages

---
+++ 1_Foundations of AI Engineering/3_Python  Hands-On/indore.py
@@ -0,0 +1,10 @@
+msg = "Indore is the best city in the world"
+
+pi = 3.14
+
+def vijay():
+    return "Vijay is a great person"
+
+def nagar(n):
+    return n + " Nagar is the best"
+
---
+++ 1_Foundations of AI Engineering/3_Python  Hands-On/main.py
@@ -0,0 +1,18 @@
+# print("Hello from Main")
+
+# import indore
+
+# print(indore.msg)
+# print(indore.pi)
+
+# print(indore.vijay())
+
+# print(indore.nagar("United States of America"))
+
+
+
+from statisticsmodels import descriptive
+
+object = descriptive.Descript()
+
+print(object.mean([1, 2, 3, 4, 5]))
